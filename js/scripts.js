ba1240fe8b373cb5df71e315c209f8c93e4a57c9
Joe Pritchett <joempritchett@gmail.com>
2019-02-01 12:33:01 -0800

    add pizza cost prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
-function  Pizza(crust, sauce, cheese, meats, veggies){
+function Pizza(crust, sauce, cheese, meats, veggies){
   this.crust = crust;
   this.sauce = sauce;
   this.cheese = cheese;
   this.meats = meats;
   this.veggies = veggies;
 }
+
+Pizza.prototype.cost = function() {
+  var cost = 0;
+  console.log(cost)
+  if (this.crust == "reg") {
+  	cost += 10
+  } else if (this.crust == "stuffed"){
+  	cost += 12
+  }
+  if (this.sauce == "red" || this.sauce == "white") {
+  	cost += 1
+  }
+  if (this.cheese == true) {
+  	cost += 1
+  }
+  cost += (2*(this.meats.length))
+  cost += (3*(this.veggies.length))
+  console.log(cost)
+}
+
+
+
 
 
 $(document).ready(function(){
